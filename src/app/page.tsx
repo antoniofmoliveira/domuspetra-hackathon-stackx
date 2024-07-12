f0088c29cbdc4619fa323022774ffd234883bdeb
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-07-12 15:02:40 -0300

    implements authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import Link from "next/link";
+
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import { signOut } from "@/auth";
+
 import {
   ReactElement,
   JSXElementConstructor,
@@ -30,6 +35,17 @@ export default function Home() {
         <Par text="Administração" target="/dashboard" />
         <Par text="Administração Blog" target="/dashboard/blog" />
         <Par text="Administração Usuários" target="/dashboard/users" />
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
       </div>
     </main>
   );
