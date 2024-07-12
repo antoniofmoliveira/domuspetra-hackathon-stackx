714fd9eb4e2cf0f64bd183c70fc9ed45740de5b2
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-07-12 05:07:24 -0300

    initial commit

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Domus Petra",
+  description: "Consultoria e treinamento",
+};
+
+export default function PublicLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
