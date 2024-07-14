d8e3845b419483a5e03beb19d3c4a14ee22c669c
Igor <igorberaldomo@gmail.com>
2024-07-14 12:23:44 -0300

    Card

--- src/app/(public)/blog/page.tsx
+++ src/app/(public)/blog/page.tsx
@@ -1,3 +1,7 @@
+import Card from "./card"
 export default function Blog() {
-  return <div>Blog Page</div>;
+   const content = (
+      <Card img="/src/app/(public)/blog/26_29-10-2014_33d8b45781.jpg" title="Ah, meu Brasil!" subtitle="Como fica o Brasil passadas as eleições de 2014?"/>
+   )
+  return content;
 }
