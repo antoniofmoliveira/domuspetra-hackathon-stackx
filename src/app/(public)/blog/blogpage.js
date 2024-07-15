eebb18455aeee2c1bcc9fee5d568e3bea4835eb9
Igor <igorberaldomo@gmail.com>
2024-07-15 17:11:48 -0300

    fix blogpage em varios arquivos

--- src/app/(public)/blog/blogpage.js
+++ src/app/(public)/blog/blogpage.js
@@ -4,5 +4,5 @@ const text = document.getElementById("text")
 
 title.innerHTML = "Ah, meu Brasil!"
 subtitle.innerHTML = "Como fica o Brasil passadas as eleições de 2014?"
-text.innerHTML =""
+// text.innerHTML =""
 
