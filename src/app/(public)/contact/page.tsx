e636cf6e3f27f7c82a3e749bd37bd5ed1dcf063e
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-07-17 19:31:42 -0300

    add suspense to contact page useSearchParams

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -2,8 +2,9 @@
 import ContactCard from "@/app/components/contact-card";
 import ContactUsForm from "@/app/components/contactus-form";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-export default function ContactPage() {
+function ContactArgs() {
   const searchParams = useSearchParams();
   const param = searchParams.get("subject");
   const arg = param == null ? "" : param;
@@ -12,5 +13,13 @@ export default function ContactPage() {
     <div>
       <ContactUsForm subject={arg} />
     </div>
+  );
+}
+
+export default function ContactPage() {
+  return (
+    <Suspense>
+      <ContactArgs />
+    </Suspense>
   );
 }
