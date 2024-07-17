ebd308395a2e532909d2fd0743eb33fdd6c513b5
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-07-17 19:13:55 -0300

    PageProps ajusted

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactCard from "@/app/components/contact-card";
 import ContactUsForm from "@/app/components/contactus-form";
 import { useSearchParams } from "next/navigation";
 interface PageProps {
-  subject?: string;
+  subject: string;
 }
 export default function Contact({ subject = "" }: PageProps) {
   const searchParams = useSearchParams();
