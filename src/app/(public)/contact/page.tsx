6f9bb158e2acc1829b4872f1c22e4c33486b4ed4
antoniofmoliveira <antoniofmoliveia@outlook.com>
2024-07-17 19:17:50 -0300

    pageprops eliminated

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -2,13 +2,11 @@
 import ContactCard from "@/app/components/contact-card";
 import ContactUsForm from "@/app/components/contactus-form";
 import { useSearchParams } from "next/navigation";
-interface PageProps {
-  subject: string;
-}
-export default function Contact({ subject = "" }: PageProps) {
+
+export default function Contact() {
   const searchParams = useSearchParams();
   const param = searchParams.get("subject");
-  const arg = param == null ? subject : param;
+  const arg = param == null ? "" : param;
 
   return (
     <div>
